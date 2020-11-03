7a94018f4ebc9f9f0fa05b556e7b8620f9339c2f
Rockronnie <roni.jumpponen@gmail.com>
2020-11-03 12:37:05 +0200

    First push

---
+++ test.py
@@ -0,0 +1,11 @@
+import requests
+
+url = 'http://127.0.0.1:1080/predict'  # localhost and the defined port + endpoint
+body = {
+    "petal_length": 4,
+    "sepal_length": 4,
+    "petal_width": 1,
+    "sepal_width": 2
+}
+response = requests.post(url, data=body)
+print(response.json())
