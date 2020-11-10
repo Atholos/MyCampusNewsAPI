2335e55088d0f5215e5392215045e2db223697a9
Rockronnie <roni.jumpponen@gmail.com>
2020-11-10 15:08:33 +0200

    Fail

--- resources/predict.py
+++ resources/predict.py
@@ -3,13 +3,14 @@ from flask_restful import Api, Resource, reqparse
 import joblib
 import numpy as np
 
-IRIS_MODEL = joblib.load('iris.mdl')
+#IRIS_MODEL = joblib.load('iris.mdl')
 
 
 class Predict(Resource):
 
     @staticmethod
     def post():
+        '''
         parser = reqparse.RequestParser()
         parser.add_argument('petal_length')
         parser.add_argument('petal_width')
@@ -19,7 +20,9 @@ class Predict(Resource):
         args = parser.parse_args()  # creates dict
 
         X_new = np.fromiter(args.values(), dtype=float)  # convert input to array
-
-        out = {'Prediction': IRIS_MODEL.predict([X_new])[0]}
+        '''
+        out = {"Prediction": "iris-setosa"} 
+        
+        ##{'Prediction': IRIS_MODEL.predict([X_new])[0]}
 
         return out, 200
