67171d1f55be62d28dc6f568647ad75cecd596e7
Rockronnie <roni.jumpponen@gmail.com>
2020-12-08 18:50:13 +0200

    Testing workflow

--- app/utils/parse_news.py
+++ app/utils/parse_news.py
@@ -4,29 +4,46 @@ from PIL import Image
 
 class Parse_News():
 
+    title = "This is article title"
+    description = "this is news parser"
+    highlight = False
+    headerImgUrl = "header Url"
+    headerImageTitle = "header image title"
+
+    author = {
+                "displayName": "This is display name",
+                "email": "this is email",
+                "department": "this is department",
+                "jobtitle": "this is job title"
+            }
+
+    paragraphs = {}
 
     newsItem = {            
         "paragraphs":{
             
         }
     }
+    image = {
+        "filename": "image filename",
+        "height": "image height",
+        "width": "image width" 
+    }
+
     def createArticle():
         article = {
-            "id": self.article.id,
             "title": self.article.title,
             "author": {
-                "id": self.author.id,
-                "displayName": self.author.displayName,
-                "email": self.author.email,
-                "department": self.author.department,
-                "jobtitle": self.author.jobtitle
+                "displayName": self.author["displayName"],
+                "email": self.author["email"],
+                "department": self.author["department"],
+                "jobtitle": self.author["jobtitle"]
             },
-            "highlight": self.article.highlight,
-            "description": self.article.description,
-            "created_at": self.article.created_at.strftime("%d-%b-%Y (%H:%M:%S.%f)"),
+            "highlight": self.highlight,
+            "description": self.description,
             "headerImgUrl": self.headerurl,
-            "headerImgTitle": self.headerImage.title,
-            "paragraphs": self.dictparagraphs,
+            "headerImgTitle": self.headerImageTitle,
+            "paragraphs": self.paragraphs,
         }
         return article
 
@@ -36,8 +53,20 @@ class Parse_News():
     def parse_news(self, doc):
         i = 0
         image_paragraphs = []
+        for image in document.inline_shapes: 
+            image_paragraphs.append(image._inline.graphic.graphicData.pic.nvPicPr.cNvPr.name)
+            print(image.height.cm, image.width.cm, image._inline.graphic.graphicData.pic.nvPicPr.cNvPr.name)
+
+
         for para in doc.paragraphs:
             rels = para.part.rels
+
+            if para.style.name == "Heading 1":
+                title = para.text
+            elif para.style.name == "Image" and i == 0:
+                headerImgUrl = 
+
+             
             newpara = {
                 "text": para.text,
                 "imgUrl": None,
@@ -45,18 +74,18 @@ class Parse_News():
                 "style": para.style.name,
             }
             if 'graphicData' in para._p.xml:
-                image_paragraphs.append(para)   
-            self.newsItem["paragraphs"][i] = newpara
+                image_paragraphs.append(para)
+               
+            self.paragraphs[i] = newpara
             i += 1
         rels = document.part.rels
         print(image_paragraphs)
-        print(self.newsItem)
+        #print(self.newsItem)
         for rel in rels:
             if rels[rel].reltype == RT.HYPERLINK:
                 print("link: ", rel, "url: ", rels[rel]._target)
-        for image in document.inline_shapes:
-            #print("hi")    
-            print (image.height.cm, image.width.cm, image._inline.graphic.graphicData.pic.nvPicPr.cNvPr.name)
+        #for image in document.inline_shapes: 
+            #print (image.height.cm, image.width.cm, image._inline.graphic.graphicData.pic.nvPicPr.cNvPr.name)
 
    
 filename = 'Joulupuu - Christmas Tree Campaign 2020 at Espoo Campus until Dec 7.docx'
